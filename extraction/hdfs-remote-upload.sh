1a78e62d6dcd1824b94eb2ca6fd7a4afd4b0ad34
Maxime Augier <max@xolus.net>
2014-03-21 11:27:25 +0100

    Use gzip instead of bz2, much faster and we do not need block decompression much

--- extraction/hdfs-remote-upload.sh
+++ extraction/hdfs-remote-upload.sh
@@ -3,8 +3,8 @@
 # Upload a single chunk to an HDFS path
 
 PATH="/team16/tweets/"
-BZ2="/bin/bzip2"
+BZ2="/bin/gzip -c"
 SSH="/usr/bin/ssh"
 
-echo "$BZ2 | $SSH icdatasrv4.epfl.ch hadoop dfs -put - \"$PATH/$1.bz2\"" >&2
-$BZ2 | $SSH icdatasrv4.epfl.ch hadoop dfs -put - "$PATH/$1.bz2"
+echo "$BZ2 | $SSH icdatasrv4.epfl.ch hadoop dfs -put - \"$PATH/$1.gz\"" >&2
+$BZ2 | $SSH icdatasrv4.epfl.ch hadoop dfs -put - "$PATH/$1.gz"
