06e1c16ff79a6422d41a69a79f08bb0d8bb2ba8b
Maxime Augier <max@xolus.net>
2014-04-07 16:34:38 +0200

    Statistics over entropy estimation

---
+++ text-entropy/mapper.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2
+
+import json
+import math
+import re
+import sys
+
+
+def entropy(seq):
+    bins = {}
+    total = 0
+    for x in seq:
+        bins[x] = 1 + bins.get(x,0)
+        total += 1
+
+    return 0 - sum((float(p) / total) * math.log(float(p) / total, 2) for p in bins.itervalues())
+
+
+if __name__ == "__main__":
+  bins = {}
+  for line in sys.stdin:
+    try:
+        tweet = json.loads(line)
+        e = entropy(tweet['text'])
+        e2 = int(e * 1000)
+        bins[e2] = 1 + bins.get(e2,0)
+
+    except KeyError:
+        continue
+        # text was missing
+    except ValueError:
+        # json decoding failed ?
+        continue
+  for (k,v) in bins.iteritems():
+    print("%s\t%s" % (k,v))
