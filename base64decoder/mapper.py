5038fecc31ab54845cf477838e42db4e5905eb24
Maxime Augier <max@xolus.net>
2014-05-04 20:56:56 +0200

    Increase selectivity of base64

--- base64decoder/mapper.py
+++ base64decoder/mapper.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python2
 
 import json
+import re
 import sys
 from base64 import b64decode
+
+chk = re.compile(r"^\s*[A-Za-z0-9+=]+\s*$")
 
 for line in sys.stdin:
     try:
         x = json.loads(line)
         text = x['text']
+
+        if not chk.match(text):
+            continue
+
+
         dec = b64decode(x['text'])
         cars = set()
         for c in dec:
             cars.add(c)
 
-        if len(cars) > 4:
-            x['b64decoded'] = dec
-            sys.stdout.write(json.dumps(x) + "\n")
+        if len(cars) < 5:
+            continue
+
+        if not any(x.isalpha() for x in cars):
+            continue
+
+        x['b64decoded'] = dec
+        sys.stdout.write(json.dumps(x) + "\n")
 
     except Exception:
         continue
